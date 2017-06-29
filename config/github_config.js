e55413ead3f93e1b5d73b9cc8a6c99ba8dc0aa16
usmanjamil83 <usmanjamil83@hotmail.com>
2017-06-29 19:32:38 -0400

    test

--- config/github_config.js
+++ config/github_config.js
@@ -2,7 +2,7 @@ var ids = {
   github: {
     clientID: '673bf3b8a0ecfccfebea',
     clientSecret: '905d7fd2a30a830e65b7d27876b6bccf98a57716',
-    callbackURL: "http://localhost:443/auth/github/profile"
+    callbackURL: "http://localhost:443/profile"
   },
 };
 
