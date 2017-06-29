ef17facbcdc336edd985c5d8f7916b6be0576818
usmanjamil83 <usmanjamil83@hotmail.com>
2017-06-29 10:34:35 -0400

    worked on github login code

---
+++ config/github_config.js
@@ -0,0 +1,9 @@
+var ids = {
+  github: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+};
+
+module.exports = ids;
