87df17d8a5cacd3dccc1786f2320565216699997
usmanjamil83 <usmanjamil83@hotmail.com>
2017-07-08 13:02:59 -0400

    test

--- config/github_config.js
+++
@@ -1,9 +0,0 @@
-var ids = {
-  github: {
-    clientID: '673bf3b8a0ecfccfebea',
-    clientSecret: '905d7fd2a30a830e65b7d27876b6bccf98a57716',
-    callbackURL: "https://gitupp.herokuapp.com/profile"
-  },
-};
-
-module.exports = ids;
